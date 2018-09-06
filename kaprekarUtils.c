428a4baca3d5ba9b9dea105c1159c198518e8705
vmorenolozano2 <42661306+vmorenolozano2@users.noreply.github.com>
2018-09-06 15:35:41 -0700

    Add files via upload
    I changed false into 0 line 28
    I changed n into square line 34
    I changed modulous into modulus line 44
    I changed numberOfDigits into numDigits line 41
    I changed 10 into line 36

---
+++ kaprekarUtils.c
@@ -0,0 +1,58 @@
+#include<math.h>
+
+#include "kaprekarUtils.h"
+
+/**
+ * <p>This function tests whether the given input <code>n</code>
+ * is a Kaprekar Number or not.  A Kaprekar Number is a non-negative
+ * integer n whose square can be split into two positive integers 
+ * whose sum is equal to the original number.  For the purposes of 
+ * this method, we restrict our consideration to base-10 representations.</p>
+ * 
+ * For example, 45 is a Kaprekar number, because 
+ *   45<sup>2</sup> = 2025 
+ * and 
+ *   20 + 25 = 45. 
+ * 
+ * <p>See Wikipedia's  
+ * <a href="https://en.wikipedia.org/wiki/Kaprekar_number">Kaprekar Number</a>
+ * article for more information.</p>
+ * 
+ * @param n
+ * @return <code>true</code> if <code>n</code> is a Kaprekar Number, 
+ * <code>false</code> otherwise.
+ */
+int isKaprekar(int n) {
+
+  if(n < 1) {
+    return 0;
+      //You can't return in false bacause youa are working with an integer
+  }
+
+  int i;
+  long square = n * (long) n;
+  int numDigits = (int) log10(square) + 1;
+    // We want to run program with the square so for example we write 45 it will rum numbers between 0 and 45 so we want the square
+  long modulus = 1;
+    //modulus hasn't to be equal to zero because you can't divide the square into zero
+  long first, second;
+
+  //for each possible "split" of the square...
+  for(i=1; i<=numDigits; i++) {
+      //numberOfDigits was incorrect because the variable was numDigits.
+    //increase the modulus by a factor of 10
+    modulus *= 10;
+     //modulous was incorrect because of the spelling , so the variable was modulus.
+    //split the square into two parts
+    first = square / modulus;
+    second = square % modulus;
+
+    //test if the split makes a Kaprekar number
+    if(second > 0 &&
+       first + second == n) {
+      return 1;
+    }
+  }
+  return 0;
+  
+}
